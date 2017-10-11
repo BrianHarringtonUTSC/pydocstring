77e0ddabe93ad9932cfad7007af30ffd795165b1
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-10-10 23:53:12 -0400

    add 2 pieces sample docstring

--- pydocstring.py
+++ pydocstring.py
@@ -25,3 +25,44 @@ class Requirement():
 
 class Example():
     pass
+
+
+if __name__ == "__main__":
+    def func1(a_str, a_int, a_float, a_list, a_dict):
+        ''' (str, int, float, list of str, dict of {str: int}) -> bool
+        This is a sample doc, this line is not too long.
+        This line is a bit longer than expected, we need to break this
+        line into two.
+
+        REQ: this is a requirement
+        REQ: another requirement
+        requirement: possibly another requirement like this
+
+        >>> func1('a', 1, 0.8, ['hello', 'world'], {'hi': 8})
+        True
+        >>> func1('a', 1, 0.8, ['hello', 'world'], {'hi': 9})
+        False
+        '''
+        return False  # dummy code
+
+
+    def func2(a_str, a_int, a_float, a_list, a_dict):
+        ''' (str, int, float, list of str, dict of {str: int}) -> bool
+        REQ: this is a requirement
+        REQ: another requirement
+        requirement: possibly another requirement like this
+
+        This is another sample doc, this possibly requirements stated before description.
+        This line is a bit longer than expected, we need to break this
+        line into two.
+
+        >>> func2('a', 1, 0.8, ['hello', 'world'], {'hi': 8})
+        True
+        >>> func2('a', 1, 0.8, ['hello', 'world'], {'hi': 9})
+        False
+        '''
+        return False  # dummy code
+
+
+    print(func1.__doc__)
+    print(func2.__doc__)
